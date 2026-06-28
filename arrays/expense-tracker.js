64344dac981fb15d1c3d481f74fc3d936071b7e7
Sean Naddeo <seannadd@gmail.com>
2026-06-28 04:44:47 +0200

    Last part arrays

--- arrays/expense-tracker.js
+++ arrays/expense-tracker.js
@@ -2,21 +2,35 @@ const account =
 {
     name: "Sean",
     expenses: [],
+    income: [],
     addExpense: function (description, amount) {
         this.expenses.push({ description, amount });
     },
+    addIncome: function (description, amount) {
+        this.income.push({ description, amount });
+    },
+    calculateExpenses: function () {
+        const balance = this.expenses.reduce((total, expense) => total + expense.amount, 0);
+        return balance;
+    },
+    calculateIncome: function () {
+        const balance = this.income.reduce((total, income) => total + income.amount, 0);
+        return balance;
+    },
     calculateBalance: function () {
-        const balance = this.expenses.reduce((total, expense) => total + expense.amount * -1, 0);
+        const balance = this.calculateIncome() - this.calculateExpenses();
         return balance;
     },
     getAccountSummary: function () {
         const balance = this.calculateBalance();
-        return `${this.name}'s balance is: ${balance}`
+        return `${this.name}'s balance is: ${balance}. Income: ${this.calculateIncome()}. Expenses: ${this.calculateExpenses()}`;
     }
 };
 
 account.addExpense("Rent", 950);
 account.addExpense("Coffee", 2);
 account.addExpense("Groceries", 150);
+account.addIncome("Salary", 2000);
+account.addIncome("Side Hustle", 500);
 
-console.log(account.getAccountSummary()); // Sean's balance is: -1102
+console.log(account.getAccountSummary()); // Sean's balance is: -1102. Income: 2500. Expenses: -1102
