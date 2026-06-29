11daae478c72368148886f3e75868f0bd9a69b99
Sean Naddeo <seannadd@gmail.com>
2026-06-29 06:50:53 +0200

    DOM Manipulation stuff

--- javascript-course/src/notes-app.js
+++ javascript-course/src/notes-app.js
@@ -17,13 +17,38 @@ const notes = [
     }
 ];
 
+const addRemoveNoteListener = ((noteIndex, button) => {
+    button.addEventListener('click', () => {
+        notes.splice(noteIndex, 1);
+        pAppender(false)
+    })
+})
 
-const pAppender = () => {
-    notes.forEach((note) => {
+const pAppender = (firstRender = true) => {
+    if(!firstRender){
+        document.querySelectorAll('p').forEach((e) => e.remove())
+        document.querySelectorAll('button').forEach((e) => e.remove())
+    }
+    notes.forEach((note, index) => {
         const newP = document.createElement('p');
+        newP.className = 'note';
         newP.textContent = note.title + " - " + note.body;
-        document.querySelector('body').appendChild(newP);
+
+        const removeNote = document.createElement('button');
+        removeNote.textContent = 'Remove note';
+        removeNote.className = 'noteRemoveBtn';
+        removeNote.id = index;
+        addRemoveNoteListener(index, removeNote);
+
+        document.querySelector('body').appendChild(newP).appendChild(removeNote);
     });
+
+    const createNote = document.createElement('button');
+    createNote.textContent = "New Note";
+    createNote.id = "create-note";
+    document.querySelector('body').appendChild(createNote);
+
+    createNote.addEventListener('click', (e) => console.log("Create note clicked"));
 };
 
 pAppender();
--- javascript-course/src/todo.js
+++ javascript-course/src/todo.js
@@ -1,13 +1,15 @@
 const todos =
-    [{ title: "Todo 1", body: "Rent apartment Brussels", completed: false },
-    { title: "Todo 2", body: "Clean the house", completed: true },
-    { title: "Todo 3", body: "Update CV", completed: false },
-    { title: "Todo 4", body: "Buy groceries", completed: true },
-    { title: "Todo 5", body: "Call mom", completed: false }];
+    [{ title: "1", body: "Rent apartment Brussels", completed: false },
+    { title: "2", body: "Clean the house", completed: true },
+    { title: "3", body: "Update CV", completed: false },
+    { title: "4", body: "Buy groceries", completed: true },
+    { title: "5", body: "Call mom", completed: false }];
+
 
 const pAppender = (replace = false) => {
     if (replace) {
         document.querySelectorAll('p').forEach((p) => p.remove());
+        document.querySelector('#createTodoBtn').remove();
     }
 
     todos.sort((item1, item2) =>
@@ -25,7 +27,9 @@ const pAppender = (replace = false) => {
         p.appendChild(c);
         document.querySelector('body').appendChild(p);
     });
-
+    
+    addButton();
+    
     addListeners();
 };
 
@@ -39,6 +43,18 @@ const addListeners = () => {
             }
         };
     });
+};
+
+const addButton = () => {
+    const button = document.createElement('button');
+    button.textContent = "Add Todo";
+    button.id = 'createTodoBtn';
+    button.onclick = () => {
+        const newTodo = { title: `Todo ${todos.length + 1}`, body: `New todo ${todos.length + 1}`, completed: false };
+        todos.push(newTodo);
+        pAppender(true);
+    };
+    document.querySelector('body').appendChild(button);
 };
 
 pAppender();
